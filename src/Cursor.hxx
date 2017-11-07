1ffe04275a2cd839b5dadf2434438c986ba29bf7
Max Kellermann <mk@cm4all.com>
2017-11-07 21:17:10 +0100

    Cursor: add API documentation

--- src/Cursor.hxx
+++ src/Cursor.hxx
@@ -16,6 +16,12 @@ class FullRecordList;
 class PerSiteRecordList;
 class Record;
 
+/**
+ * An iterator for records in the #Database.  While an instance
+ * exists, the database may be modified, because
+ * #Record::DisplaceCursors() will take care for cleaning up
+ * soon-to-be-invalid pointers.
+ */
 class Cursor final
 	: public boost::intrusive::list_base_hook<boost::intrusive::link_mode<boost::intrusive::auto_unlink>> {
 
@@ -30,12 +36,25 @@ public:
 	Cursor(Database &_database, const Filter &filter,
 	       BoundMethod<void()> _append_callback);
 
+	/**
+	 * Rewind to the first record.
+	 */
 	void Rewind();
 
+	/**
+	 * Enable "follow" mode: the #Database will call OnAppend() as
+	 * soon as a new record gets added.
+	 */
 	void Follow();
 
+	/**
+	 * Callback invoked by the #Database.
+	 */
 	void OnAppend(const Record &record);
 
+	/**
+	 * Does this instance point to a valid record?
+	 */
 	operator bool() const {
 		return next != nullptr;
 	}
@@ -54,6 +73,9 @@ public:
 		return next;
 	}
 
+	/**
+	 * Skip to the next record.
+	 */
 	Cursor &operator++();
 };
 
