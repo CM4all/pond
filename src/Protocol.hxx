361f2458dec687f3739c34b14a133c26ebb284a0
Max Kellermann <mk@cm4all.com>
2017-12-04 14:01:46 +0100

    Protocol: assign numbers to commands

--- src/Protocol.hxx
+++ src/Protocol.hxx
@@ -7,48 +7,48 @@
 #include <stdint.h>
 
 enum class PondRequestCommand : uint16_t {
-	NOP,
+	NOP = 0,
 
 	/**
 	 * Commit the current request parameters and start executing
 	 * the request.
 	 */
-	COMMIT,
+	COMMIT = 1,
 
 	/**
 	 * Cancel this request.
 	 */
-	CANCEL,
+	CANCEL = 2,
 
 	/**
 	 * Query records.  This packet initiates a new request.
 	 */
-	QUERY,
+	QUERY = 3,
 
 	/**
 	 * Specify a filter on the "site" attribute.  Payload is the
 	 * exact string to compare with.
 	 */
-	FILTER_SITE,
+	FILTER_SITE = 4,
 
 	/**
 	 * Option for #QUERY which follows incoming new records
 	 * instead of printing past ones.  The response never ends
 	 * until the client sends #CANCEL (or closes the connection).
 	 */
-	FOLLOW,
+	FOLLOW = 5,
 
 	/**
 	 * Specify a filter on the "timestamp" attribute.  Payload is
 	 * a 64 bit time stamp (microseconds since epoch).
 	 */
-	FILTER_SINCE,
+	FILTER_SINCE = 6,
 
 	/**
 	 * Specify a filter on the "timestamp" attribute.  Payload is
 	 * a 64 bit time stamp (microseconds since epoch).
 	 */
-	FILTER_UNTIL,
+	FILTER_UNTIL = 7,
 
 	/**
 	 * Option for #QUERY which limits the number of distinct sites
@@ -56,28 +56,28 @@ enum class PondRequestCommand : uint16_t {
 	 * files while keeping only a certain number of files open.
 	 * Payload is #PondGroupSitePayload.
 	 */
-	GROUP_SITE,
+	GROUP_SITE = 8,
 };
 
 enum class PondResponseCommand : uint16_t {
-	NOP,
+	NOP = 0,
 
 	/**
 	 * An error has occurred.  Payload is a human-readable error
 	 * message.
 	 */
-	ERROR,
+	ERROR = 1,
 
 	/**
 	 * End of the current response.  Needed for some types of
 	 * responses.
 	 */
-	END,
+	END = 2,
 
 	/**
 	 * A serialized log record according to net/log/Protocol.hxx
 	 */
-	LOG_RECORD,
+	LOG_RECORD = 3,
 };
 
 /**
