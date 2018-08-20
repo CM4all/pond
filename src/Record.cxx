aaceffb1acdae22e6f877d01f7ecdca72a2072f4
Max Kellermann <mk@cm4all.com>
2018-08-20 17:44:37 +0200

    Record: cast to void* to work around -Wclass-memaccess

--- src/Record.cxx
+++ src/Record.cxx
@@ -38,7 +38,7 @@
 Record::Record(uint64_t _id, ConstBuffer<uint8_t> _raw)
 	:id(_id), raw_size(_raw.size)
 {
-	memcpy(this + 1, _raw.data, raw_size);
+	memcpy((void *)(this + 1), _raw.data, raw_size);
 
 	auto raw = ConstBuffer<uint8_t>::FromVoid({this + 1, raw_size});
 	parsed = Net::Log::ParseDatagram(raw.begin(), raw.end());
