c32b025cbb7acd81fb4a56d3e19fa399fb4f93bb
Max Kellermann <mk@cm4all.com>
2018-09-04 13:14:40 +0200

    meson.build: make documentation build optional

--- build/configure.py
+++ build/configure.py
@@ -13,6 +13,7 @@ flavors = {
     'asan': {
         'options': [
             '-Db_sanitize=address',
+            '-Ddocumentation=false',
         ],
     },
 
@@ -20,6 +21,7 @@ flavors = {
         'options': [
             '--buildtype', 'release',
             '-Db_ndebug=true',
+            '-Ddocumentation=false',
         ],
         'env': {
             'CFLAGS': '-ffunction-sections -fdata-sections',
@@ -33,11 +35,13 @@ flavors = {
             '--buildtype', 'release',
             '-Db_ndebug=true',
             '-Db_lto=true',
+            '-Ddocumentation=false',
         ],
     },
 
     'clang': {
         'options': [
+            '-Ddocumentation=false',
         ],
         'env': {
             'CC': 'clang',
