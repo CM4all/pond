d555504c251382cf977aa27ebc03b449ce7c2bf1
Max Kellermann <mk@cm4all.com>
2019-11-08 13:32:39 +0100

    client/Client: larger receive buffer

--- src/client/Client.hxx
+++ src/client/Client.hxx
@@ -49,7 +49,7 @@ class PondClient {
 
 	uint16_t last_id = 0;
 
-	StaticFifoBuffer<uint8_t, 65536> input;
+	StaticFifoBuffer<uint8_t, 256 * 1024> input;
 
 public:
 	explicit PondClient(UniqueSocketDescriptor &&_fd) noexcept
